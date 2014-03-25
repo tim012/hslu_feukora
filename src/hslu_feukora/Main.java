f94ea5f7bb36b4181603bf2aa43b16ffdde58a11
Tim <Tim@timspc.burkhard.local>
2014-03-25 18:13:40 +0100

    tested some stuff

--- src/hslu_feukora/Main.java
+++ src/hslu_feukora/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Das totale Projekt");
+		
+		// again!
 		System.out.println("wahnsinn"); // dies ist ein patch!
-
 	}
 
 }
