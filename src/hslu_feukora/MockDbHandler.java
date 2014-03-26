8076f51769ab25a4d21197099d767201ba4db21a
localhost21 <alexander.horst.ah@gmail.com>
2014-03-26 10:26:00 +0100

    Mock DB

--- src/hslu_feukora/MockDbHandler.java
+++ src/hslu_feukora/MockDbHandler.java
@@ -1,20 +1,28 @@
 package hslu_feukora;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
 public class MockDbHandler {
-	
-	private String username;
-	private String password;
-	
-	private static EntityManagerFactory entityManagerFactory = null;
 
-	static {
+	public String username;
+	public String password;
+
+	public static EntityManagerFactory emf = null;
+
+	public void createDb() {
+
 		try {
 			/* EntityManagerFactory erzeugen */
-			entityManagerFactory = Persistence
-					.createEntityManagerFactory("DemoPU");
+			emf = Persistence.createEntityManagerFactory("DemoPU");
+		} catch (Throwable e) {
+			/* TODO - Fehlerbehandlung … */
+			e.printStackTrace();
+		}
 
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+	}
 }
