6d7c7192fc146ee18b1031cd091f397d32ed48af
localhost21 <alexander.horst.ah@gmail.com>
2014-03-26 09:36:38 +0100

    neuer Kommentar

--- src/hslu_feukora/Main.java
+++ src/hslu_feukora/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Das totale Projekt");
-		
+
 		// again!
 		System.out.println("wahnsinn"); // dies ist ein patch!
+		System.out.println("neu von Alex geschrieben");
 	}
 
 }
